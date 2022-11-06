5296dd873c29a182508cb84f536bdf0aac8eedc6
Tomisin Babalola <tomisin.keji.babalola@gmail.com>
2022-11-06 00:40:08 -0700

    Update on the  project style

---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPU44KzV8saVt8u7VtwH81s1zxoQnLy_c",
+  authDomain: "altschool-project-b676c.firebaseapp.com",
+  projectId: "altschool-project-b676c",
+  storageBucket: "altschool-project-b676c.appspot.com",
+  messagingSenderId: "607211802850",
+  appId: "1:607211802850:web:1ac47261215dee8800cebb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
